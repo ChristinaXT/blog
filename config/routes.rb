6c7909454ff02f766d7efe29b4b8a28ee9a424a3
Guess <guess@Guesss-iMac.local>
2019-08-15 10:57:24 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
-  get 'about', to: 'pages#about'
-  get 'signup', to: 'users#new'
-  resources :articles
-  resources :users, except: [:new]
+ resources :articles
+root 'pages#index'
+   get 'about', to: 'pages#about'
+   get 'signup', to: 'users#new'
+   resources :users, except: [:new]
+   get 'login', to: 'sessions#new'
+   post 'login', to: 'sessions#create'
+ delete 'logout', to: 'sessions#destroy'
 end
